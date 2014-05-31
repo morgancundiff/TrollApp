781704cd2053d49a2f97f1c96d4e9dd8bf0679ef
morgancundiff <mcundiff@ucsd.edu>
2014-05-30 18:19:06 -0700

    Create FavoritesActivity.java

---
+++ Troll/src/edu/ucsd/troll/app/FavoritesActivity.java
@@ -0,0 +1,105 @@
+package edu.ucsd.troll.app;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.app.ActionBar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+
+import java.util.HashMap;
+
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import edu.ucsd.troll.app.R;
+
+/**
+* Created on 5/30/2014.
+*/
+public class ProfileActivity extends Activity {
+
+    //init the progress bar
+    private ProgressDialog pDialog;
+
+    // URL to get menu JSON
+    private static String url = "http://troll.everythingcoed.com/user/login";
+
+    // JSON Node names
+    private static final String TAG_CONTACTS = "menu";
+    private static final String TAG_ID = "id";
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_DESCRIPTION = "description";
+    private static final String TAG_CATEGORY = "category";
+    private static final String TAG_RATING = "rating";
+    private static final String TAG_SIZES = "sizes";
+    private static final String TAG_SIZE = "size";
+    private static final String TAG_PRICE = "price";
+    //private static final String TAG_PHONE_OFFICE = "office";
+
+    // menu JSONArray
+    JSONArray menu = null;
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> favoriteList;
+    
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      setContentView(R.layout.favorites_layout);
+    }
+    
+    final Button backButton = (Button) findViewById(R.id.backProfBtn);
+    
+    backButton.setOnClickListener(new View.onClickListener){
+      @override
+      
+      public void onClick(View view){
+        Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivity(i);
+        finish();
+      }
+    });
+}
